c8baeedf6c776b74460c92c59ef9d8b5605ffd7e
Igor Iakovlev <elediablo@yandex.ru>
2026-09-16 10:19:18 +0500

    v

--- cats/models.py
+++ cats/models.py
@@ -27,6 +27,15 @@ class Cat(models.Model):
         User, related_name='cats', on_delete=models.CASCADE)
     achievements = models.ManyToManyField(Achievement, through='AchievementCat')
 
+    class Meta:
+        unique_together = ('name', 'owner')
+        contraints = [
+            models.UniqueConstraint(
+                fields=['name', 'owner'],
+                name='unque_name_owner'
+            )
+        ]
+
     def __str__(self):
         return self.name
 
